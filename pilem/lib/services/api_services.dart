b1d46923718e693903bacb15f4bf718d65887284
Alfa <128333121+Alfatihahrizqullaab@users.noreply.github.com>
2026-03-08 15:15:00 +0700

    pilem, karyawan

---
+++ pilem/lib/services/api_services.dart
@@ -0,0 +1,44 @@
+// API KEY: 5a163cbae74e827afd8420c1f612c7bc
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService{
+  static const String baseUrl = 'https://api.themoviedb.org/3';
+  // ganti dengan api key
+  static const String apiKey = '5a163cbae74e827afd8420c1f612c7bc';
+   //1. mengambil list movie yang saat ini tayang
+  Future<List<Map<String, dynamic>>> getAllMovies() async {
+    final response = await http.get(
+      Uri.parse("$baseUrl/movie/now_playing?api_key=$apiKey"),
+    );
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);
+  }
+  
+  //2. mengambil list movie yang sedang trending minggu ini
+  Future<List<Map<String, dynamic>>> getTrendingMovies() async {
+    final response = await http.get(
+      Uri.parse("$baseUrl/trending/movie/week?api_key=$apiKey"),
+    );
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);
+  }
+
+  //3. mengambil list popular movie 
+  Future<List<Map<String, dynamic>>> getPopularMovies() async {
+    final response = await http.get(
+      Uri.parse("$baseUrl/movie/popular?api_key=$apiKey"),
+    );
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);
+  }
+
+  //4. mengambil list movie melalui pencarian
+  Future<List<Map<String, dynamic>>> searchMovies(String query) async {
+    final response = await http.get(
+      Uri.parse("$baseUrl/search/movie?query=$query&api_key=$apiKey"),
+    );
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);
+  }
+}
